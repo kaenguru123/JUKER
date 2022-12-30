03fa8089de495c54536a32d471c99b8cf9a841e0
FCell1 <julian.schoenfeld@outlook.com>
2022-12-30 11:21:18 +0100

    Daten-->JSON

--- Juker/Juker/View/Registration.xaml.cs
+++ Juker/Juker/View/Registration.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Juker.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Juker.View
 {
@@ -20,9 +24,42 @@ namespace Juker.View
     /// </summary>
     public partial class Registration : UserControl
     {
+        string path = "A:/Downloads/customer_data.json"; //individuell anpassen
         public Registration()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(path)
+            {
+                InsertData(path);
+            }
+            else
+            {
+                File.Create(path).Close();
+                InsertData(path);
+                
+            }
+            
+            
+        }
+        private void InsertData(string FilePath)
+        {
+            Customer newCustomer = new Customer();
+            newCustomer.FirstName = "";
+            newCustomer.LastName = "";
+            newCustomer.PhoneNumber = "";
+            newCustomer.Email = "";
+            newCustomer.PictureUrl = "";
+            newCustomer.Company = null;
+            newCustomer.ProductIntrests = null;
+
+            var initialJson = File.ReadAllText(FilePath);
+            var existingCustomer = JArray.Parse(initialJson);
+            existingCustomer.Add(newCustomer);
+            var jsonToOutput = JsonConvert.SerializeObject(existingCustomer, Formatting.Indented);
         }
     }
 }
