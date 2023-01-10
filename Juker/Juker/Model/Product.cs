a77d55096f9e81a5f5fdb7169f9232c82bf1e2df
kaenguru123 <daskaenguru1234@gmail.com>
2023-01-10 19:25:12 +0100

    changes ;)

--- Juker/Juker/Model/Product.cs
+++ Juker/Juker/Model/Product.cs
@@ -9,7 +9,7 @@ namespace Juker.Model
 {
     public class Product
     {
-              public int Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
 
