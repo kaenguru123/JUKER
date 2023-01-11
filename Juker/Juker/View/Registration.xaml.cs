989c7690ba3d5682250d998053a4cecdd52a5dd2
Kenrick <Kenrick@KENRICK-PC>
2023-01-11 16:49:04 +0100

    Added exception handling when no camera is found or when starting the camera throws an exception.

--- Juker/Juker/View/Registration.xaml.cs
+++ Juker/Juker/View/Registration.xaml.cs
@@ -25,217 +25,232 @@ using Image = System.Drawing.Image;
 
 namespace Juker.View
 {
-  /// <summary>
-  /// Interaktionslogik für Registration.xaml
-  /// </summary>
-  public partial class Registration : UserControl
-  {
-    #region Members
-    private VideoCaptureDevice videoCaptureDevice;
-    private FilterInfoCollection infoCollection;
-
-    private string pathProduct { get; set; }
-    private string pathCustomer { get; set; }
-
-    private List<Product> productList;
-    private List<Product> customerInterests;
-    private bool isCompanyCustomer;
-    private BitmapImage bitmapImage;
-    private MemoryStream memoryStream;
-    private string downloadDirectory = Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders", "{374DE290-123F-4565-9164-39C4925E467B}", String.Empty).ToString();
-    #endregion
-
-    #region Constructor
-    public Registration()
+    /// <summary>
+    /// Interaktionslogik für Registration.xaml
+    /// </summary>
+    public partial class Registration : UserControl
     {
-      InitializeComponent();
-      CompanyExtensionHead.Visibility = Visibility.Collapsed;
-      CompanyExtension.Visibility = Visibility.Collapsed;
-      SaveButton.Visibility = Visibility.Collapsed;
+        #region Members
+        private VideoCaptureDevice videoCaptureDevice;
+        private FilterInfoCollection infoCollection;
 
-      customerInterests = new List<Product>();
+        private string pathProduct { get; set; }
+        private string pathCustomer { get; set; }
 
+        private List<Product> productList;
+        private List<Product> customerInterests;
+        private bool isCompanyCustomer;
+        private BitmapImage bitmapImage;
+        private MemoryStream memoryStream;
+        private string downloadDirectory = Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders", "{374DE290-123F-4565-9164-39C4925E467B}", String.Empty).ToString();
+        #endregion
 
-      var productDestDir = ConfigurationManager.AppSettings["product.json"] ?? "";
-      pathProduct = downloadDirectory + productDestDir + "\\product.json";
-
-      var customerDestDir = ConfigurationManager.AppSettings["customer.json"] ?? "";
-      pathCustomer = downloadDirectory + customerDestDir + "\\customer.json";
-
-      if (!(File.Exists(pathProduct)))
-      {
-        File.Create(pathProduct).Close();
-      }
-      try
-      {
-        var jsonDataProduct = File.ReadAllText(pathProduct);
-        List<Product> productList = JsonConvert.DeserializeObject<List<Product>>(jsonDataProduct);
-        ProductListView.ItemsSource = productList;
-      }
-      catch (Exception ex)
-      {
-        MessageBoxHelper.throwErrorMessageBox(
-            ex.Message + "\nNo products in customer.json or wrong path to customer.json.",
-            "Could not retrieve list of product from product.json");
-      }
-    }
-    #endregion 
-
-    #region HelpMethods
-
-    private void InsertData(string FilePath)
-    {
-      Company company = new Company();
-      if (isCompanyCustomer)
-      {
-        company.Name = CompanyName.Text;
-        company.Street = CompanyStreet.Text;
-        company.HouseNumber = CompanyHouseNumber.Text;
-        company.City = CompanyCity.Text;
-        company.Country = CompanyCountry.Text;
-      }
-
-      Customer newCustomer = new Customer()
-      {
-        FirstName = FirstName.Text,
-        LastName = LastName.Text,
-        PhoneNumber = PhoneNumber.Text,
-        Email = Email.Text,
-        PictureUrl = "",
-        Company = company,
-        ProductInterests = customerInterests
-      };
-      StreamReader reader = new StreamReader(FilePath);
-      string initialJson = reader.ReadToEnd();
-      reader.Close();
-      var list = initialJson != "" ? JsonConvert.DeserializeObject<List<Customer>>(initialJson) : new List<Customer>();
-      list?.Add(newCustomer);
-      string customerList = JsonConvert.SerializeObject(list, Formatting.Indented);
-      File.WriteAllText(FilePath, customerList);
-    }
-    private void FinaleFrame_newFrame(object sender, NewFrameEventArgs eventArgs)
-    {
-      try
-      {
-        using (var bitmap = (Bitmap)eventArgs.Frame.Clone())
+        #region Constructor
+        public Registration()
         {
-          bitmapImage = new BitmapImage();
-          bitmapImage.BeginInit();
-          memoryStream = new MemoryStream();
-          bitmap.Save(memoryStream, ImageFormat.Bmp);
-          bitmapImage.StreamSource = memoryStream;
-          bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-          bitmapImage.EndInit();
+            InitializeComponent();
+            CompanyExtensionHead.Visibility = Visibility.Collapsed;
+            CompanyExtension.Visibility = Visibility.Collapsed;
+            SaveButton.Visibility = Visibility.Collapsed;
+
+            customerInterests = new List<Product>();
+
+
+            var productDestDir = ConfigurationManager.AppSettings["product.json"] ?? "";
+            pathProduct = downloadDirectory + productDestDir + "\\product.json";
+
+            var customerDestDir = ConfigurationManager.AppSettings["customer.json"] ?? "";
+            pathCustomer = downloadDirectory + customerDestDir + "\\customer.json";
+
+            if (!(File.Exists(pathProduct)))
+            {
+                File.Create(pathProduct).Close();
+            }
+            try
+            {
+                var jsonDataProduct = File.ReadAllText(pathProduct);
+                List<Product> productList = JsonConvert.DeserializeObject<List<Product>>(jsonDataProduct);
+                ProductListView.ItemsSource = productList;
+            }
+            catch (Exception ex)
+            {
+                MessageBoxHelper.throwErrorMessageBox(
+                    ex.Message + "\nNo products in customer.json or wrong path to customer.json.",
+                    "Could not retrieve list of product from product.json");
+            }
         }
-        bitmapImage.Freeze();
-        Dispatcher.BeginInvoke(new ThreadStart(delegate
-        { Webcam.Source = bitmapImage; }));
+        #endregion
 
+        #region HelpMethods
 
-      }
-      catch (Exception ex)
-      {
-        //catch your error here
-      }
-    }
-    #endregion
-
-    #region CheckBoxHandlers
-    private void CompanyCheckBoxChecked(object sender, RoutedEventArgs e)
-    {
-      CompanyExtensionHead.Visibility = Visibility.Visible;
-      CompanyExtension.Visibility = Visibility.Visible;
-      isCompanyCustomer = true;
-    }
-    private void CompanyCheckBoxUnchecked(object sender, RoutedEventArgs e)
-    {
-      CompanyExtension.Visibility = Visibility.Collapsed;
-      CompanyExtensionHead.Visibility = Visibility.Collapsed;
-      isCompanyCustomer = false;
-    }
-
-    private void ProductCheckBoxChecked(object sender, RoutedEventArgs e)
-    {
-      if (sender != null)
-      {
-        CheckBox checkBox = (CheckBox)sender;
-        Product selectedProduct = (Product)checkBox.DataContext;
-        if (!customerInterests.Contains(selectedProduct))
+        private void InsertData(string FilePath)
         {
-          customerInterests.Add(selectedProduct);
+            Company company = new Company();
+            if (isCompanyCustomer)
+            {
+                company.Name = CompanyName.Text;
+                company.Street = CompanyStreet.Text;
+                company.HouseNumber = CompanyHouseNumber.Text;
+                company.City = CompanyCity.Text;
+                company.Country = CompanyCountry.Text;
+            }
+
+            Customer newCustomer = new Customer()
+            {
+                FirstName = FirstName.Text,
+                LastName = LastName.Text,
+                PhoneNumber = PhoneNumber.Text,
+                Email = Email.Text,
+                PictureUrl = "",
+                Company = company,
+                ProductInterests = customerInterests
+            };
+            StreamReader reader = new StreamReader(FilePath);
+            string initialJson = reader.ReadToEnd();
+            reader.Close();
+            var list = initialJson != "" ? JsonConvert.DeserializeObject<List<Customer>>(initialJson) : new List<Customer>();
+            list?.Add(newCustomer);
+            string customerList = JsonConvert.SerializeObject(list, Formatting.Indented);
+            File.WriteAllText(FilePath, customerList);
         }
-      }
-    }
-
-    private void ProductCheckBoxUnchecked(object sender, RoutedEventArgs e)
-    {
-      if (sender != null)
-      {
-        CheckBox checkBox = (CheckBox)sender;
-        Product selectedProduct = (Product)checkBox.DataContext;
-        customerInterests.Remove(selectedProduct);
-      }
-    }
-
-    #endregion
-
-    #region ButtonHandlers
-
-    private void StartCamera(object sender, RoutedEventArgs e)
-    {
-      infoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
-      videoCaptureDevice = new VideoCaptureDevice(infoCollection[0].MonikerString);
-      videoCaptureDevice.NewFrame += FinaleFrame_newFrame;
-      videoCaptureDevice.Start();
-      SaveButton.Visibility = Visibility.Visible;
-      ImagePlaceHolder.Visibility = Visibility.Collapsed;
-    }
-    private void SaveImage(object sender, RoutedEventArgs e)
-    {
-      try
-      {
-        videoCaptureDevice.NewFrame -= FinaleFrame_newFrame;
-        Image img = System.Drawing.Image.FromStream(memoryStream);
-        videoCaptureDevice.SignalToStop();
-
-        img.Save(downloadDirectory + "\\image.jpeg", ImageFormat.Jpeg);
-        Webcam.Source = new BitmapImage(new Uri(downloadDirectory + "\\image.jpeg"));
-        img.Dispose();
-        SaveButton.Visibility = Visibility.Collapsed;
-      }
-      catch (Exception ex)
-      { 
-        MessageBoxHelper.throwErrorMessageBox(
-            ex.Message + "\nAn error ccured while capturing your image. Please take one again.",
-            "saving error");
-      }
-      finally
-      {
-        memoryStream.Dispose();
-      }
-    }
-    private void SubmitButtonClick(object sender, RoutedEventArgs e)
-    {
-      try
-      {
-        if (!(File.Exists(pathCustomer)))
+        private void FinaleFrame_newFrame(object sender, NewFrameEventArgs eventArgs)
         {
-          File.Create(pathCustomer).Close();
+            try
+            {
+                using (var bitmap = (Bitmap)eventArgs.Frame.Clone())
+                {
+                    bitmapImage = new BitmapImage();
+                    bitmapImage.BeginInit();
+                    memoryStream = new MemoryStream();
+                    bitmap.Save(memoryStream, ImageFormat.Bmp);
+                    bitmapImage.StreamSource = memoryStream;
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapImage.EndInit();
+                }
+                bitmapImage.Freeze();
+                Dispatcher.BeginInvoke(new ThreadStart(delegate
+                { Webcam.Source = bitmapImage; }));
+
+
+            }
+            catch (Exception ex)
+            {
+                //catch your error here
+            }
         }
-        InsertData(pathCustomer);
-      }
-      catch (Exception ex)
-      {
-        MessageBoxHelper.throwErrorMessageBox(
-            ex.Message + "\ngive all mandatory information.",
-            "Invalid input");
-      }
+        #endregion
+
+        #region CheckBoxHandlers
+        private void CompanyCheckBoxChecked(object sender, RoutedEventArgs e)
+        {
+            CompanyExtensionHead.Visibility = Visibility.Visible;
+            CompanyExtension.Visibility = Visibility.Visible;
+            isCompanyCustomer = true;
+        }
+        private void CompanyCheckBoxUnchecked(object sender, RoutedEventArgs e)
+        {
+            CompanyExtension.Visibility = Visibility.Collapsed;
+            CompanyExtensionHead.Visibility = Visibility.Collapsed;
+            isCompanyCustomer = false;
+        }
+
+        private void ProductCheckBoxChecked(object sender, RoutedEventArgs e)
+        {
+            if (sender != null)
+            {
+                CheckBox checkBox = (CheckBox)sender;
+                Product selectedProduct = (Product)checkBox.DataContext;
+                if (!customerInterests.Contains(selectedProduct))
+                {
+                    customerInterests.Add(selectedProduct);
+                }
+            }
+        }
+
+        private void ProductCheckBoxUnchecked(object sender, RoutedEventArgs e)
+        {
+            if (sender != null)
+            {
+                CheckBox checkBox = (CheckBox)sender;
+                Product selectedProduct = (Product)checkBox.DataContext;
+                customerInterests.Remove(selectedProduct);
+            }
+        }
+
+        #endregion
+
+        #region ButtonHandlers
+
+        private void StartCamera(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                infoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                videoCaptureDevice = new VideoCaptureDevice(infoCollection[0].MonikerString);
+                videoCaptureDevice.NewFrame += FinaleFrame_newFrame;
+                videoCaptureDevice.Start();
+                SaveButton.Visibility = Visibility.Visible;
+                ImagePlaceHolder.Visibility = Visibility.Collapsed;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                MessageBoxHelper.throwErrorMessageBox(
+                              "An error ccured while starting your camera. \nDo you have a camera installed?",
+                               "Start camera error");
+            }
+            catch (Exception ex)
+            {
+                MessageBoxHelper.throwErrorMessageBox(
+                 "An error ccured while starting your camera. Please try again. \n" + ex.Message,
+                    "Start camera error");
+            }
+        }
+        private void SaveImage(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                videoCaptureDevice.NewFrame -= FinaleFrame_newFrame;
+                Image img = System.Drawing.Image.FromStream(memoryStream);
+                videoCaptureDevice.SignalToStop();
+
+                img.Save(downloadDirectory + "\\image.jpeg", ImageFormat.Jpeg);
+                Webcam.Source = new BitmapImage(new Uri(downloadDirectory + "\\image.jpeg"));
+                img.Dispose();
+                SaveButton.Visibility = Visibility.Collapsed;
+            }
+            catch (Exception ex)
+            {
+                MessageBoxHelper.throwErrorMessageBox(
+                    ex.Message + "\nAn error ccured while capturing your image. Please take one again.",
+                    "saving error");
+            }
+            finally
+            {
+                memoryStream.Dispose();
+            }
+        }
+        private void SubmitButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!(File.Exists(pathCustomer)))
+                {
+                    File.Create(pathCustomer).Close();
+                }
+                InsertData(pathCustomer);
+            }
+            catch (Exception ex)
+            {
+                MessageBoxHelper.throwErrorMessageBox(
+                    ex.Message + "\ngive all mandatory information.",
+                    "Invalid input");
+            }
+        }
+
+        #endregion
+
+
+
     }
-
-    #endregion
-
-
-
-  }
 }
 
